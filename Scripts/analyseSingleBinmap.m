de3fcdb396262d6dea4c860982b4ffbb3665d863
Mathias Pechinger <ge64was@mytum.de>
2023-06-14 11:01:40 +0200

    minor changes analyser

--- Scripts/analyseSingleBinmap.m
+++ Scripts/analyseSingleBinmap.m
@@ -1,48 +1,20 @@
 function analyseSingleBinmap(binMapName,osmDataName,MapX,MapY,bPlotParkedVeh)
-% analyseSingleBinmapHeatspots: Analyzes a given binary map and open street map (OSM) data and visualizes the heatmap.
-%
-% This function loads the input binary map and OSM data, plots the buildings from OSM data on the same coordinates 
-% as the binary map, and uses colors to represent the binary map's values as a heatmap.
-% Each pixel of the binary map corresponds to a rectangular area on the plotted map. The colors of these areas 
-% range from white (if the binary map's value at that pixel is zero) to the color corresponding to the maximum 
-% value on the colormap parula (if the value is non-zero).
-%
-% Usage:
-%   analyseSingleBinmapHeatspots(binMapName,osmDataName,MapX,MapY)
-%
-% Input:
-%   binMapName: A string that specifies the name of the .mat file containing the binary map data.
-%   osmDataName: A string that specifies the name of the .mat file containing the OSM data.
-%   MapX: A two-element vector [Xmin Xmax] that defines the x-axis limits of the map.
-%   MapY: A two-element vector [Ymin Ymax] that defines the y-axis limits of the map.
-%
-% Notes:
-%   The binary map data should be in the form of a 2D matrix stored in the 'binmap' variable in the .mat file.
-%   The OSM data should include building information stored in the 'osmBuildings' variable in the .mat file.
-%
-%   This function creates a full-screen figure and uses the parula colormap to visualize the data.
-%   The heatmap visualization uses normalization for color mapping. The delay between plotting each 
-%   pixel can be adjusted by changing the pause time.
-%
-%   In the OSM data, each building is represented as a structure with x and y coordinates.
-%
-%   It should be noted that a value of zero in the binary map is displayed as white, and the colors for 
-%   other values are scaled according to the maximum value in the binary map.
-
+    % Load the binary map and OSM data
     load(binMapName);
     load(osmDataName);
-       
+    
+    % Create a full-screen figure and set axis equal
     figure('units','normalized','outerposition',[0 0 1 1])
     axis equal
     axis([MapX(1) MapX(2) MapY(1) MapY(2)])
     hold on
     
+    % Plot buildings from the OSM data
     for building = 1:size(osmBuildings,2)
         plot(osmBuildings{building}.x,osmBuildings{building}.y,'k')
     end
 
     if (bPlotParkedVeh)
-    % only for static occlusion scenario
         for parking = 1:size(osmParking,2)
             plot(osmParking{parking}.x,osmParking{parking}.y,'k')
         end
@@ -50,7 +22,7 @@ function analyseSingleBinmap(binMapName,osmDataName,MapX,MapY,bPlotParkedVeh)
 
     hold on
 
-    % get max value for color normalization
+    % Get max value for color normalization
     maxBinValue = max(max(binmap));
     cmap = colormap(parula);
     
@@ -60,7 +32,7 @@ function analyseSingleBinmap(binMapName,osmDataName,MapX,MapY,bPlotParkedVeh)
             boxPointX = zeros(1,4);
             boxPointY = zeros(1,4);
 
-            % start at bottom left
+            % Start at bottom left
             boxPointX(1) = [MapX(1)+xIter];
             boxPointX(2) = [MapX(1)+xIter];
             boxPointX(3) = [MapX(1)+xIter+1];
@@ -83,7 +55,6 @@ function analyseSingleBinmap(binMapName,osmDataName,MapX,MapY,bPlotParkedVeh)
 
             % if it IS NOT empty
             else              
-                % colorCode = ceil((1-(currValue/maxBinValue))*255) ;
                 colorCode = ceil(((currValue/maxBinValue))*255) ;
     
                 % avoid invalid 0
@@ -95,5 +66,11 @@ function analyseSingleBinmap(binMapName,osmDataName,MapX,MapY,bPlotParkedVeh)
         end
         pause(0.0001) 
     end
-end
+    
+    % Add labels to x and y axis
+    xlabel('x position [m]', 'FontName', 'Times New Roman')
+    ylabel('y position [m]', 'FontName', 'Times New Roman')
 
+    % Set the current axes font to Times New Roman
+    set(gca, 'FontName', 'Times New Roman')
+end
